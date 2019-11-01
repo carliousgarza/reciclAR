a45c23b60e24ae1cf58dcfc878dc03ad29e3aeb5
Carlos <carlosfgarza@gmail.com>
2019-11-01 09:26:57 -0600

    confetti and ui

--- ReciclAR/ScoreViewController.swift
+++ ReciclAR/ScoreViewController.swift
@@ -18,6 +18,7 @@ class ScoreViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         lbScore.text = "You got a score of \(score!)!"
+        createParticles()
         // Do any additional setup after loading the view.
     }
     
@@ -25,7 +26,40 @@ class ScoreViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    
+    func createParticles() {
+        let particleEmitter = CAEmitterLayer()
+
+        particleEmitter.emitterPosition = CGPoint(x: view.center.x, y: 0)
+        particleEmitter.emitterShape = .line
+        particleEmitter.emitterSize = CGSize(width: view.frame.size.width, height: 1)
+
+        let red = makeEmitterCell(color: UIColor.red)
+        let green = makeEmitterCell(color: UIColor.green)
+        let blue = makeEmitterCell(color: UIColor.blue)
+
+        particleEmitter.emitterCells = [red, green, blue]
+
+        view.layer.addSublayer(particleEmitter)
+    }
+
+    func makeEmitterCell(color: UIColor) -> CAEmitterCell {
+        let cell = CAEmitterCell()
+        cell.birthRate = 3
+        cell.lifetime = 7.0
+        cell.lifetimeRange = 0
+        cell.color = color.cgColor
+        cell.velocity = 200
+        cell.velocityRange = 50
+        cell.emissionLongitude = CGFloat.pi
+        cell.emissionRange = CGFloat.pi / 4
+        cell.spin = 2
+        cell.spinRange = 3
+        cell.scaleRange = 0.5
+        cell.scaleSpeed = -0.05
+
+        cell.contents = UIImage(named: "rectangle")?.cgImage
+        return cell
+    }
 
     /*
     // MARK: - Navigation
